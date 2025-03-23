9369adf6e6d0985f643809440283eee3bb9ff749
JuanPGalli <jpgallir@gmail.com>
2025-03-23 01:20:11 -0300

    added styles to /NavBar responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+/* import { Link, useLocation } from "react-router-dom";
 import f1Logo from "/Logo-f1.png";
 import "./Navbar.css";
 
@@ -16,6 +16,37 @@ const Navbar = () => {
 
         {location.pathname !== "/" && location.pathname !== "/create" && (
           <Link to={"/create"}>CREATE DRIVER</Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar; */
+import { Link, useLocation } from "react-router-dom";
+import f1Logo from "/Logo-f1.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const location = useLocation();
+  return (
+    <div className="nav-container">
+      <div className="img-container">
+        <Link to={"/"}>
+          <img src={f1Logo} alt="formula1Logo" />
+        </Link>
+      </div>
+      <div className="link-container">
+        {location.pathname !== "/home" && (
+          <Link to={"/home"} className="home-button">
+            HOME
+          </Link>
+        )}
+
+        {location.pathname !== "/" && location.pathname !== "/create" && (
+          <Link to={"/create"} className="create-button">
+            CREATE DRIVER
+          </Link>
         )}
       </div>
     </div>
